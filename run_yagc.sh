df28b87525c8929546c471c46aae7af6ea6107bd
rvosa <rutgeraldo@gmail.com>
2013-11-21 10:35:03 +0100

    updated paths to data files, now uses runner class instead of yagc.pl

--- run_yagc.sh
+++ run_yagc.sh
@@ -4,16 +4,16 @@
 VERBOSE=
 
 # gff3 directory, must NOT exist if the intention is to re-explode the GFF3
-GFF3DIR=gff3
+GFF3DIR=share/gff3
 
 # directory for tbl and fasta files
-TBLDIR=tblfasta-new
+TBLDIR=share/tblfasta-new
 if [ ! -d $TBLDIR ]; then
 	mkdir -p $TBLDIR
 fi
 
 # validation directory, contains val reports, ASN.1 output (and genbank files)
-ASN1DIR=asn1val-new
+ASN1DIR=share/asn1val-new
 if [ ! -d $ASN1DIR ]; then
 	mkdir -p $ASN1DIR
 fi
@@ -26,16 +26,16 @@ SOURCE=maker
 PREFIX=L345_
 
 # input genome
-FASTA=king_cobra_scaffolds_spring_2011-cleaned.fasta
+FASTA=share/king_cobra_scaffolds_spring_2011-cleaned.fasta
 
 # input annotation file
-GFF3=cobra.functional-cleaned.gff
+GFF3=share/cobra.functional-cleaned.gff
 
 # number of contigs to chunk together in one file (<=10k)
 CHUNKSIZE=5000
 
 # extra metadata to include in the fasta header lines
-INFO=info.ini
+INFO=share/info.ini
 
 # AGP settings
 CENTER=NaturalisBC
@@ -64,21 +64,21 @@ MINLENGTH=200
 MININTRON=10
 
 # discrepancy report file, compare with https://www.ncbi.nlm.nih.gov/genbank/asndisc
-DISCREP=discrep-new.txt
+DISCREP=share/discrep-new.txt
 
 # the submission template as produced by http://www.ncbi.nlm.nih.gov/WebSub/template.cgi
-TEMPLATE=template.sbt
+TEMPLATE=share/template.sbt
 
 # explode the annotations into files 
 if [ ! -d $GFF3DIR ]; then	
 	mkdir -p $GFF3DIR
-	perl explode_gff3.pl -gff3 $GFF3 -source $SOURCE -f CDS -f gene -f five_prime_UTR \
-		-f three_prime_UTR -d $GFF3DIR
+	perl -Ilib script/explode_gff3.pl -gff3 $GFF3 -source $SOURCE -f CDS -f gene \
+	-f five_prime_UTR -f three_prime_UTR -d $GFF3DIR
 fi
 
 # create the table and fasta files
-perl -Ilib yagc.pl -d $TBLDIR -s $SOURCE -p $PREFIX -f $FASTA -g $GFF3DIR \
- 	-info $INFO -a $AUTHORITY -c $CHUNKSIZE -l $LIMIT -minlength $MINLENGTH \
+perl -Ilib -MBio::WGS2NCBI -e run - -d $TBLDIR -s $SOURCE -p $PREFIX -f $FASTA \
+	-g $GFF3DIR -info $INFO -a $AUTHORITY -c $CHUNKSIZE -l $LIMIT -minlength $MINLENGTH \
  	-minintron $MININTRON $VERBOSE 
 
 # create the genbank and validation files
