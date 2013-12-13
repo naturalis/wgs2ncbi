cb790e187f86aa850fb1048744c6d088354777c1
rvosa <rutgeraldo@gmail.com>
2013-12-13 17:23:46 +0100

    added POD

--- lib/Bio/WGS2NCBI/CdsFeature.pm
+++ lib/Bio/WGS2NCBI/CdsFeature.pm
@@ -2,8 +2,44 @@ package Bio::WGS2NCBI::CdsFeature;
 use Bio::WGS2NCBI::MrnaFeature;
 use base 'Bio::WGS2NCBI::MrnaFeature';
 
+=head1 NAME
+
+Bio::WGS2NCBI::CdsFeature - coding sequence feature
+
+=head1 DESCRIPTION
+
+An object of this class represents all CDS features
+of a single gene.
+
+=head1 SEE ALSO
+
+L<Bio::WGS2NCBI::MrnaFeature>
+
+=head1 METHODS
+
+=over
+
+=item qualifiers()
+
+Returns the qualifiers to include in the feature table for a CDS. These
+include all qualifiers used in a L<Bio::WGS2NCBI::MrnaFeature>, as well as
+'note', 'codon_start', and 'db_xref'.
+
+=cut
+
 sub qualifiers { shift->SUPER::qualifiers, qw(note codon_start db_xref) }
 
+=item phase()
+
+Returns the GFF3 'phase', i.e. the number of nucleotides after which the
+next reading frame begins (0, 1 or 2).
+
+=cut
+
 sub phase { shift->{'phase'} }
+
+=back
+
+=cut
 
 1;
--- lib/Bio/WGS2NCBI/MrnaFeature.pm
+++ lib/Bio/WGS2NCBI/MrnaFeature.pm
@@ -2,6 +2,35 @@ package Bio::WGS2NCBI::MrnaFeature;
 use Bio::WGS2NCBI::StrandedFeature;
 use base 'Bio::WGS2NCBI::StrandedFeature';
 
+=head1 NAME
+
+Bio::WGS2NCBI::MrnaFeature - mRNA feature
+
+=head1 DESCRIPTION
+
+Objects of this class represent an mRNA feature for a gene.
+
+=head1 SEE ALSO
+
+L<Bio::WGS2NCBI::StrandedFeature>
+
+=head1 METHODS
+
+=over
+
+=item new()
+
+Returns a new mRNA feature. Requires the arguments 'product', 'protein_id' and 
+'transcript_id', for example:
+
+ my $mrna = Bio::WGS2NCBI::MrnaFeature->new(
+ 	product       => $product,
+ 	protein_id    => $protein_id,
+ 	transcript_id => $transcript_id,
+ );
+
+=cut
+
 sub new {
     my $class = shift;
     my %args = @_;
@@ -11,6 +40,17 @@ sub new {
     $class->SUPER::new(%args);  
 }
 
+=item qualifiers()
+
+Returns the feature qualifiers for mRNA features, i.e. 'product', 'protein_id' and 
+'transcript_id'
+
+=cut
+
 sub qualifiers { qw(product protein_id transcript_id) }
+
+=back
+
+=cut
 
 1;
