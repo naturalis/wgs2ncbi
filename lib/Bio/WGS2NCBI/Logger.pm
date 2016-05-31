2ab075d13db4e35393038bf600817dff8fa465f2
Rutger Vos <rutgeraldo@gmail.com>
2016-05-31 12:19:43 +0200

    added ERROR level, colorized messages, simplified logging

--- lib/Bio/WGS2NCBI/Logger.pm
+++ lib/Bio/WGS2NCBI/Logger.pm
@@ -1,20 +1,45 @@
 package Bio::WGS2NCBI::Logger;
+use Term::ANSIColor;
 require Exporter;
 use base 'Exporter';
 
-our $Verbosity = 1;
-our @EXPORT = qw(DEBUG INFO WARN);
+our $Complexity = 0;
+our $Verbosity  = 1;
+our @EXPORT      = qw(DEBUG INFO WARN ERROR);
+our %COLORS      = (
+	'DEBUG' => 'blue', 
+	'INFO'  => 'green',
+	'WARN'  => 'yellow',
+	'ERROR' => 'red',
+);
 
 sub LOG ($$) {
     my ($msg,$method) = @_;
-    my ( $package, $file1up, $line1up, $sub ) = caller( 2 );
-    my ( $pack0up, $file, $line, $sub0up )    = caller( 1 );
-    my $log = sprintf( "%s %s [%s %s] - %s\n", uc $method, $sub || '', $0, $line, $msg );
-    print STDERR $log;
+
+	# compose the message. Complex messages do some amount of introspection
+	# to figure out where the message originated.
+    my $log;
+    if ( $Complexity ) {
+		my ( $package, $file1up, $line1up, $sub ) = caller( 2 );
+		my ( $pack0up, $file, $line, $sub0up )    = caller( 1 );
+		$log = sprintf( "%s %s [%s %s] - %s\n", uc $method, $sub || '', $file, $line, $msg );
+    }
+    else {
+    	$log = $method . ' ' . $msg . "\n";    	
+    }
+    
+    # check if we are actually writing to the terminal rather than a file
+    if ( -t STDERR ) {
+    	print STDERR colored( $log, $COLORS{$method} );
+    }
+    else {
+    	print STDERR $log;
+    }
     return $Verbosity;
 }
 sub DEBUG ($) { LOG shift, 'DEBUG' if $Verbosity >= 3 }
 sub INFO ($)  { LOG shift, 'INFO'  if $Verbosity >= 2 }
 sub WARN ($)  { LOG shift, 'WARN'  if $Verbosity >= 1 }
+sub ERROR ($) { LOG shift, 'ERROR' if $Verbosity >= 0 }
 
 1;
