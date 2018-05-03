25c63f7ae40933a5746311565f8bde459eec3e07
Rutger Vos <rutgeraldo@gmail.com>
2018-05-03 14:36:02 +0200

    Update wgs2ncbi_convert.sh

--- galaxy/wgs2ncbi_convert.sh
+++ galaxy/wgs2ncbi_convert.sh
@@ -7,7 +7,9 @@ outdir=$tempdir/outdir
 datadir=$tempdir/datadir
 mkdir $outdir
 mkdir $datadir
-tar -xzf $3 -C $datadir --strip-components=1
+#tar -xzf $3 -C $datadir --strip-components=1
+unzip -qq -d $datadir $3
 wgs2ncbi convert $1 -datadir $datadir -outdir $outdir
-tar -C $outdir -cvzf $2 . > /dev/null 2>&1
+#tar -C $outdir -cvzf $2 . > /dev/null 2>&1
+zip -jrq $2 $outdir
 rm -rf $tempdir > /dev/null 2>&1
