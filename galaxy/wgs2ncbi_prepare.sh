f7367f7f25c3bed4d8606737fce1190fa2d1c05f
Rutger Vos <rutgeraldo@gmail.com>
2018-05-03 14:56:34 +0200

    Update wgs2ncbi_prepare.sh

--- galaxy/wgs2ncbi_prepare.sh
+++ galaxy/wgs2ncbi_prepare.sh
@@ -3,5 +3,6 @@
 
 gff3dir=`mktemp -d 2>/dev/null || mktemp -d -t 'gff3dir'`
 wgs2ncbi prepare -conf $2 -gff3dir $gff3dir -gff3file $1
-zip -jrq $3 $gff3dir
+zip -jrq $3.zip $gff3dir
+mv $3.zip $3
 rm -rf $gff3dir
